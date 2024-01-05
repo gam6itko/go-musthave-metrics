aa6489cbc09beab15d717aab09455b3afe3a8143
gam6itko <gam6itko@gmail.com>
2024-01-05 21:05:45 +0300

    test

--- cmd/server/middleware.go
+++ cmd/server/middleware.go
@@ -70,19 +70,19 @@ func compressMiddleware(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		ow := w
 
-		//if slices.Contains(compressEnabledForTypeList, r.Header.Get("Accept")) {
-		//	// проверяем, что клиент умеет получать от сервера сжатые данные в формате gzip
-		//	acceptEncoding := r.Header.Get("Accept-Encoding")
-		//	supportsGzip := strings.Contains(acceptEncoding, "gzip")
-		//	if supportsGzip {
-		//		// оборачиваем оригинальный http.ResponseWriter новым с поддержкой сжатия
-		//		cw := newCompressWriter(w)
-		//		// меняем оригинальный http.ResponseWriter на новый
-		//		ow = cw
-		//		// не забываем отправить клиенту все сжатые данные после завершения middleware
-		//		defer cw.Close()
-		//	}
-		//}
+		if slices.Contains(compressEnabledForTypeList, r.Header.Get("Accept")) {
+			// проверяем, что клиент умеет получать от сервера сжатые данные в формате gzip
+			acceptEncoding := r.Header.Get("Accept-Encoding")
+			supportsGzip := strings.Contains(acceptEncoding, "gzip")
+			if supportsGzip {
+				// оборачиваем оригинальный http.ResponseWriter новым с поддержкой сжатия
+				cw := newCompressWriter(w)
+				// меняем оригинальный http.ResponseWriter на новый
+				ow = cw
+				// не забываем отправить клиенту все сжатые данные после завершения middleware
+				defer cw.Close()
+			}
+		}
 
 		if slices.Contains(compressEnabledForTypeList, r.Header.Get("Content-Type")) {
 			// проверяем, что клиент отправил серверу сжатые данные в формате gzip
