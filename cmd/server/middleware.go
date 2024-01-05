a41c16df01753c5cbd0440eb83850b28bd214eb4
gam6itko <gam6itko@gmail.com>
2024-01-05 20:33:54 +0300

    iter8

--- cmd/server/middleware.go
+++ cmd/server/middleware.go
@@ -3,6 +3,7 @@ package main
 import (
 	"go.uber.org/zap"
 	"net/http"
+	"slices"
 	"strings"
 	"time"
 )
@@ -61,34 +62,43 @@ func requestLoggingMiddleware(h http.Handler) http.Handler {
 }
 
 func compressMiddleware(h http.Handler) http.Handler {
+	compressEnabledForTypeList := []string{
+		"application/json",
+		"text/html",
+	}
+
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		ow := w
 
-		// проверяем, что клиент умеет получать от сервера сжатые данные в формате gzip
-		acceptEncoding := r.Header.Get("Accept-Encoding")
-		supportsGzip := strings.Contains(acceptEncoding, "gzip")
-		if supportsGzip {
-			// оборачиваем оригинальный http.ResponseWriter новым с поддержкой сжатия
-			cw := newCompressWriter(w)
-			// меняем оригинальный http.ResponseWriter на новый
-			ow = cw
-			// не забываем отправить клиенту все сжатые данные после завершения middleware
-			defer cw.Close()
+		if slices.Contains(compressEnabledForTypeList, r.Header.Get("Accept")) {
+			// проверяем, что клиент умеет получать от сервера сжатые данные в формате gzip
+			acceptEncoding := r.Header.Get("Accept-Encoding")
+			supportsGzip := strings.Contains(acceptEncoding, "gzip")
+			if supportsGzip {
+				// оборачиваем оригинальный http.ResponseWriter новым с поддержкой сжатия
+				cw := newCompressWriter(w)
+				// меняем оригинальный http.ResponseWriter на новый
+				ow = cw
+				// не забываем отправить клиенту все сжатые данные после завершения middleware
+				defer cw.Close()
+			}
 		}
 
-		// проверяем, что клиент отправил серверу сжатые данные в формате gzip
-		contentEncoding := r.Header.Get("Content-Encoding")
-		sendsGzip := strings.Contains(contentEncoding, "gzip")
-		if sendsGzip {
-			// оборачиваем тело запроса в io.Reader с поддержкой декомпрессии
-			cr, err := newCompressReader(r.Body)
-			if err != nil {
-				w.WriteHeader(http.StatusInternalServerError)
-				return
+		if slices.Contains(compressEnabledForTypeList, r.Header.Get("Content-Type")) {
+			// проверяем, что клиент отправил серверу сжатые данные в формате gzip
+			contentEncoding := r.Header.Get("Content-Encoding")
+			sendsGzip := strings.Contains(contentEncoding, "gzip")
+			if sendsGzip {
+				// оборачиваем тело запроса в io.Reader с поддержкой декомпрессии
+				cr, err := newCompressReader(r.Body)
+				if err != nil {
+					w.WriteHeader(http.StatusInternalServerError)
+					return
+				}
+				// меняем тело запроса на новое
+				r.Body = cr
+				defer cr.Close()
 			}
-			// меняем тело запроса на новое
-			r.Body = cr
-			defer cr.Close()
 		}
 
 		// передаём управление хендлеру
