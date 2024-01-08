eddeef772ef47728e60023309dc6dc778fb3468c
gam6itko <gam6itko@gmail.com>
2024-01-08 11:16:08 +0300

    vet fix

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -105,14 +105,11 @@ func catchSignal(server *http.Server) {
 
 	signal.Notify(terminateSignals, syscall.SIGINT, syscall.SIGTERM) //NOTE:: syscall.SIGKILL we cannot catch kill -9 as its force kill signal.
 
-	select {
-	case s := <-terminateSignals:
-		Log.Info("Got one of stop signals, shutting down server gracefully, SIGNAL NAME :", zap.String("signal", s.String()))
-		if err := MetricStorage.Save(); err != nil {
-			Log.Error(err.Error(), zap.String("event", "metrics save"))
-		}
-		err := server.Shutdown(context.Background())
-		Log.Info("Error from shutdown", zap.String("error", err.Error()))
-		break
+	s := <-terminateSignals
+	Log.Info("Got one of stop signals, shutting down server gracefully, SIGNAL NAME :", zap.String("signal", s.String()))
+	if err := MetricStorage.Save(); err != nil {
+		Log.Error(err.Error(), zap.String("event", "metrics save"))
 	}
+	err := server.Shutdown(context.Background())
+	Log.Info("Error from shutdown", zap.String("error", err.Error()))
 }
