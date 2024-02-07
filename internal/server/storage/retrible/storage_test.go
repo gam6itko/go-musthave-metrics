4bb8f44d836b1d0db145eba2822b58d01cfce925
gam6itko <gam6itko@gmail.com>
2024-02-07 12:47:31 +0300

    review changes

--- internal/server/storage/retrible/storage_test.go
+++ internal/server/storage/retrible/storage_test.go
@@ -20,28 +20,28 @@ func Test_Decoration(t *testing.T) {
 	inner.EXPECT().
 		CounterInc(context.Background(), "foo", int64(1)).
 		Return(errors.New("boom")).
-		Times(2)
+		Times(3)
 	inner.EXPECT().
 		CounterGet(context.Background(), "foo").
 		Return(int64(0), errors.New("boom")).
-		Times(2)
+		Times(3)
 	inner.EXPECT().
 		CounterAll(context.Background()).
 		Return(map[string]int64{}, errors.New("boom")).
-		Times(2)
+		Times(3)
 	// gauge
 	inner.EXPECT().
 		GaugeSet(context.Background(), "bar", 19.17).
 		Return(errors.New("boom")).
-		MinTimes(2)
+		MinTimes(3)
 	inner.EXPECT().
 		GaugeGet(context.Background(), "bar").
 		Return(float64(0.0), errors.New("boom")).
-		MinTimes(2)
+		MinTimes(3)
 	inner.EXPECT().
 		GaugeAll(context.Background()).
 		Return(map[string]float64{}, errors.New("boom")).
-		Times(2)
+		Times(3)
 
 	s := NewStorage(
 		inner,
@@ -88,25 +88,25 @@ func Test_Decoration2(t *testing.T) {
 	// counter
 	gomock.InOrder(
 		inner.EXPECT().
-			CounterInc("counter1", int64(1)).
+			CounterInc(context.Background(), "counter1", int64(1)).
 			Return(Error{}),
 		inner.EXPECT().
-			CounterInc("counter1", int64(1)).
+			CounterInc(context.Background(), "counter1", int64(1)).
 			Return(Error{}),
 		inner.EXPECT().
-			CounterInc("counter1", int64(1)).
+			CounterInc(context.Background(), "counter1", int64(1)).
 			Return(nil),
 	)
 
 	gomock.InOrder(
 		inner.EXPECT().
-			CounterGet("counter1").
+			CounterGet(context.Background(), "counter1").
 			Return(int64(0), Error{}),
 		inner.EXPECT().
-			CounterGet("counter1").
+			CounterGet(context.Background(), "counter1").
 			Return(int64(0), Error{}),
 		inner.EXPECT().
-			CounterGet("counter1").
+			CounterGet(context.Background(), "counter1").
 			Return(int64(1), nil),
 	)
 
@@ -118,10 +118,12 @@ func Test_Decoration2(t *testing.T) {
 		},
 	)
 
-	err := s.CounterInc("counter1", 1)
+	ctx := context.Background()
+
+	err := s.CounterInc(ctx, "counter1", 1)
 	assert.NoError(t, err)
 
-	val, err := s.CounterGet("counter1")
+	val, err := s.CounterGet(ctx, "counter1")
 	assert.NoError(t, err)
 	assert.Equal(t, int64(1), val)
 }
